2d6b3b960e850a116036db71ca0364c2ee91beba
Jeremy Bourne <jbourne25@yahoo.com>
2019-12-05 22:03:47 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,13 +3,34 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-class EmailAddressParser
-  def initialize(emails)
-    @emails = emails 
-  end
-  def parse
-    @emails.tr(",", "").split(" ").uniq 
-    #We are returning a string of emails and need to return an array 
-  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class EmailAddressParser
+#   def initialize(emails)
+#     @emails = emails 
+#   end
+#   def parse
+#     @emails.tr(",", "").split(" ").uniq 
+#     #We are returning a string of emails and need to return an array 
+#   end
   
-end 
+# # end 
